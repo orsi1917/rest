6f99af7c4e4421667d3e16cb5d6719113651b49c
orsi1917 <orsi1917@yahoo.co.uk>
2017-11-07 09:03:07 +0100

    properties file added

--- backend/src/main/java/com/klm/dev/exercises/devcase02/randomquote/RandomQuoteClient.java
+++ backend/src/main/java/com/klm/dev/exercises/devcase02/randomquote/RandomQuoteClient.java
@@ -1,17 +1,28 @@
 package com.klm.dev.exercises.devcase02.randomquote;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.stereotype.*;
+import org.springframework.beans.factory.annotation.*;
 import java.util.Arrays;
 
 @Service
+@Configuration
+@PropertySource("classpath:RandomQuote.properties")
 public class RandomQuoteClient {
+
    private RestTemplate restTemplate = new RestTemplate();
 
+    @Value("${url}")
+    private String url;
+
    public Quote getQuote() {
-        return restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        return restTemplate.getForObject(url, Quote.class);
     }
 
 
