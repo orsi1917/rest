e3e5c16727664218ea37bfe10db5a73eedacbef0
orsi1917 <orsi1917@yahoo.co.uk>
2017-11-08 10:59:05 +0100

    flight begin

---
+++ backend/src/main/java/com/klm/dev/exercises/devcase02/flight/Flight.java
@@ -0,0 +1,11 @@
+package com.klm.dev.exercises.devcase02.flight;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Flight {
+}
