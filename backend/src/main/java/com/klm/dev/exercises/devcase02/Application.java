ece435ef386b9e5c1cdae39841e453e11c4db193
orsi1917 <orsi1917@yahoo.co.uk>
2017-11-13 08:19:09 +0100

    changed: field names in weather current to camelcase

--- backend/src/main/java/com/klm/dev/exercises/devcase02/Application.java
+++ backend/src/main/java/com/klm/dev/exercises/devcase02/Application.java
@@ -4,9 +4,8 @@ package com.klm.dev.exercises.devcase02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
-import java.util.concurrent.Executor;
+
 
 @SpringBootApplication
 public class Application {
--- backend/src/main/java/com/klm/dev/exercises/devcase02/weather/Current.java
+++ backend/src/main/java/com/klm/dev/exercises/devcase02/weather/Current.java
@@ -1,6 +1,7 @@
 package com.klm.dev.exercises.devcase02.weather;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,13 +9,21 @@ import lombok.Setter;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Current {
-    private String last_updated;
-    private Double temp_c;
-    private  Double temp_f;
-    private Boolean is_day;
+    @JsonProperty("last_updated")
+    private String lastUpdated;
+    @JsonProperty("temp_c")
+    private Double temperatureInCelcius;
+    @JsonProperty("temp_f")
+    private  Double temperatureInFahrenheit;
+    @JsonProperty("is_day")
+    private Boolean isDay;
     private Condition condition;
-    private Double wind_mph;
-    private Double wind_kph;
-    private Double feelslike_c;
-    private Double feelslike_f;
+    @JsonProperty("wind_mph")
+    private Double windMph;
+    @JsonProperty("wind_kph")
+    private Double windKmph;
+    @JsonProperty("feelslike_c")
+    private Double feelsLikeInCelsius;
+    @JsonProperty("feelslike_f")
+    private Double feelsLikeInFahrenheid;
 }
